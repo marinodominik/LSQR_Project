068468cebe5894b21f04b29745099472368be116
Yuval Freund <yuvalfreund93@gmail.com>
2021-01-25 17:15:28 +0100

    my

--- lsqrCUDAcuSparse.h
+++ lsqrCUDAcuSparse.h
@@ -6,5 +6,5 @@
 CPUMatrix sparseLSQR_with_kernels(const CPUMatrix &A, const CPUMatrix &b, double lambda, double ebs);
 
 CPUMatrix sparseLSQR(const CPUMatrix &A, const CPUMatrix &b, double ebs);
-CPUMatrix sparseLSQR_aux(const GPUMatrix &A, const GPUMatrix &b,GPUMatrix &u,GPUMatrix &v,GPUMatrix &w,GPUMatrix &x,GPUMatrix &tempVector,double ebs);
+CPUMatrix sparseLSQR_aux(const cusparseSpMatDescr_t &A, const cusparseSpVecDescr_t &b,cusparseDnVecDescr_t &u,cusparseDnVecDescr_t &v,cusparseDnVecDescr_t &w,cusparseDnVecDescr_t &x,cusparseDnVecDescr_t &tempVector,double ebs);
 void cuSPARSECheck(cusparseStatus_t status, int line);
